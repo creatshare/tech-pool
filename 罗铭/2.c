ac0daa8f58d4867629d346ae295634c3ac8df919
Kuparuysaka <1303205793@qq.com>
2017-04-27 19:04:16 +0800

    submit

---
+++ 罗铭/2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(void)
+{
+	int day,month,year,sum,leap;
+	printf("请输入年,月,日\n");
+	scanf("%d,%d,%d",&year,&month,&day);
+	switch(month)
+	{
+	case 1:sum=0;break;
+	case 2:sum=31;break;
+	case 3:sum=59;break;
+	case 4:sum=90;break;
+	case 5:sum=120;break;
+	case 6:sum=151;break;
+	case 7:sum=181;break;
+	case 8:sum=212;break;
+	case 9:sum=243;break;
+	case 10:sum=273;break;
+	case 11:sum=304;break;
+	case 12:sum=334;break;
+	default:printf("日期错误");break;
+	}
+	sum = sum +day;
+	if(year%400 == 0||(year%4 == 0&&year%100 != 0))
+		leap=1;
+	else
+		leap=0;
+	if(leap==1&&month>2)
+		sum=sum+1;
+	printf("这是第%d天", sum);
+	return 0;
+}
---
+++ 罗铭/c.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("陈锋是瓜皮！\n");
+return 0;	
+}
