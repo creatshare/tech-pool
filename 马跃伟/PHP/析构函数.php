d607a5f500a9c564658fde04e37f3d34c202dc6b
花不休 <1347638343@qq.com>
2017-05-25 00:57:37 +0800

    php更新

---
+++ 马跃伟/PHP/析构函数.php
@@ -0,0 +1,18 @@
+<?php
+ class Book
+{
+	private $title;
+	private $isbn;
+	private $copies;
+	function __construct($isbn)
+	{
+		echo "<p>Book class instance created.</p>";
+	}
+	function __destruct()
+	{
+		echo "<p>Book class instance distroyed.</p>";
+	}
+}
+$book = new Book("0615303889");
+
+ ?>
