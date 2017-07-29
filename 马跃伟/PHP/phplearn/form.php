64ff57b3d1674908d5e7e423d078a4267afda035
花不休 <1347638343@qq.com>
2017-07-29 12:53:16 +0800

    php

---
+++ 马跃伟/PHP/phplearn/form.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DELL-XPS
+ * Date: 2017/7/24
+ * Time: 14:55
+ */
+
+//if(isset($_POST['name'])){
+//    $name = $_POST['name'];
+//    $email = $_POST['email'];
+//    printf("Hi %s<br/>",$name);
+//    printf("The address %s will soon be  a spam_magent<br/>",$email);
+//}
+//
+//<!---->
+//<!--<form action="form.php" method="post">-->
+//<!--<p>-->
+//<!--    Name:<br/>-->
+//<!--    <input type="text" id="name" name="name" size="20"  maxlength="40" />-->
+//<!--</p>-->
+//<!--    <p>-->
+//<!--        Email Address:<br/>-->
+//<!--        <input type="text" id="email" name="email" size="20"  maxlength="40" />-->
+//<!--    </p>-->
+//<!--    <input type="submit" id="submit" name="submit" value="go!" />-->
+//<!--</form>-->
+
+require_once "HTML/QuickForm.php";
+
